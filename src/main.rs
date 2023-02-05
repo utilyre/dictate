42e5c979044331954053ea5ffd194fb34738c121
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-05 22:49:17 +0330

    feat(io): prompt user for word and print it out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next();
+    let Some(word) = args.next() else {
+        println!("expected word as second argument");
+        process::exit(2);
+    };
+
+    println!("World: {word}");
 }
