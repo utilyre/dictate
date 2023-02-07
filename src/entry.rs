26eba5a9c08569ca516b2b3fd8f293c533d04904
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-07 18:53:15 +0330

    refactor(entry): move Entry struct to its own module

---
+++ src/entry.rs
@@ -0,0 +1,73 @@
+use std::fmt::{self, Display};
+
+use colored::Colorize;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Entry {
+    word: String,
+    phonetics: Vec<Phonetic>,
+    meanings: Vec<Meaning>,
+}
+
+impl Display for Entry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut stringified = format!(
+            "{}{}{}",
+            "".white(),
+            self.word.bold().black().on_white(),
+            "".white()
+        );
+
+        if let Some(phonetic) = self.phonetics.iter().find(|p| p.text.is_some()) {
+            let text = phonetic
+                .text
+                .as_ref()
+                .expect("should never be the None variant");
+
+            stringified.push_str(&format!(" {}\n", text.italic().bright_black()))
+        }
+
+        for meaning in self.meanings.iter() {
+            stringified.push_str(&format!(
+                "\n\t{}",
+                meaning.part_of_speech.italic().underline()
+            ));
+
+            for definition in meaning.definitions.iter() {
+                stringified.push_str(&format!("\n\t\t{} {}\n", "•".blue(), definition.brief));
+
+                if let Some(example) = &definition.example {
+                    stringified.push_str(&format!(
+                        "\t\t  {}\n",
+                        format!("\"{}\"", example).bright_black()
+                    ));
+                }
+            }
+        }
+
+        write!(f, "{}", stringified)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Phonetic {
+    text: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Meaning {
+    #[serde(rename = "partOfSpeech")]
+    part_of_speech: String,
+    definitions: Vec<Definition>,
+
+    synonyms: Vec<String>,
+    antonyms: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Definition {
+    #[serde(rename = "definition")]
+    brief: String,
+    example: Option<String>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod entry;
+
 mod client;
 
 mod io;
