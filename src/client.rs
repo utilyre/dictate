29e1d6d6aa741e05e8ca653661e95e994e585890
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-15 19:25:53 +0330

    fix(client): handle http 404 status

--- src/client.rs
+++ src/client.rs
@@ -1,15 +1,25 @@
 use std::error::Error;
 
+use reqwest::{Client, StatusCode};
+
 use crate::entry::Entry;
 
 pub async fn fetch_word(word: &str) -> Result<Vec<Entry>, Box<dyn Error>> {
-    let entries = reqwest::get(format!(
-        "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
-        word
-    ))
-    .await?
-    .json::<Vec<Entry>>()
-    .await?;
+    let client = Client::new();
+    let response = client
+        .get(format!(
+            "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
+            word
+        ))
+        .send()
+        .await;
 
-    Ok(entries)
+    match response {
+        Ok(response) if response.status() == StatusCode::OK => Ok(response.json().await?),
+        Ok(response) if response.status() == StatusCode::NOT_FOUND => {
+            Err(format!("word `{}` not found", word))?
+        }
+        Ok(response) => Err(format!("request failed ({})", response.status()))?,
+        Err(err) => Err(Box::new(err)),
+    }
 }
