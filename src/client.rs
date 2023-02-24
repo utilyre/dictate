85043fbe3d85add6bade2e9bc0187301dacd5aa4
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-24 13:55:20 +0330

    refactor(client): use the into method for converting string to Error

--- src/client.rs
+++ src/client.rs
@@ -16,9 +16,9 @@ pub async fn fetch_word(word: &str) -> Result<Vec<Entry>, Box<dyn Error>> {
     match response {
         Ok(response) if response.status() == StatusCode::OK => Ok(response.json().await?),
         Ok(response) if response.status() == StatusCode::NOT_FOUND => {
-            Err(format!("word `{}` not found", word))?
+            Err(format!("word `{}` not found", word).into())
         }
-        Ok(response) => Err(format!("request failed ({})", response.status()))?,
+        Ok(response) => Err(format!("request failed ({})", response.status()).into()),
         Err(err) => Err(Box::new(err)),
     }
 }
